ecf1ec54d368e26ffb0a6e45f7bb8835b15685c6
MatthewStarkGD <matthewstark322@gmail.com>
2024-06-29 23:10:46 +0600

    Scenes and Tiles

---
+++ Hackathon/Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingManager : MonoBehaviour
+{
+    public Text loadingText; 
+    public Slider loadingBar;
+    public float fakeLoadingSpeed = 0.1f;
+
+    void Start()
+    {
+        
+        StartCoroutine(LoadAsync("SampleScene")); 
+    }
+
+    IEnumerator LoadAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+        float progress = 0f;
+
+        while (!operation.isDone)
+        {
+            
+            if (progress < 1f)
+            {
+                progress += fakeLoadingSpeed * Time.deltaTime;
+                progress = Mathf.Clamp01(progress);
+            }
+
+            
+            float realProgress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            
+            float displayProgress = Mathf.Min(progress, realProgress);
+
+            
+            if (loadingText != null)
+            {
+                loadingText.text = "Loading... " + (displayProgress * 100) + "%";
+            }
+            if (loadingBar != null)
+            {
+                loadingBar.value = displayProgress;
+            }
+
+            
+            if (displayProgress >= 1f && realProgress >= 1f)
+            {
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
