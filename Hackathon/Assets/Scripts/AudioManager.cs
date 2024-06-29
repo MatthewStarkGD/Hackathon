4fb7d5f2a478bf64d49ce6256b777d75fe02f676
MatthewStarkGD <matthewstark322@gmail.com>
2024-06-30 02:00:19 +0600

    Settings audio v1

---
+++ Hackathon/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    private AudioSource[] allAudioSources;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void SetVolume(float volume)
+    {
+        AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource audioSource in allAudioSources)
+        {
+            audioSource.volume = volume;
+        }
+    }
+}
+
